9afabb0ee51076306dba1b1b99b4bb7b88d178bd
danieltonad <soladan2000@gmail.com>
2023-08-09 17:23:48 -0700

    checking out deta base functionalities

--- config/db.py
+++ config/db.py
@@ -1,7 +1,14 @@
-from deta import Deta  # Import Deta
+from deta import Deta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize
 deta = Deta()
 
 # This how to connect to or create a database.
 database = deta.Base("simple_db")
+
+# set key
+database.project_key = os.environ.get('project-key-jaybird-cl1')
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from config.db import database
+import uuid
 
 
 app = FastAPI()
@@ -6,4 +8,9 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    print(100)
+    resp = database.get()
+    return resp
+@app.get('/put')
+def add():
+    resp = database.put(data={'message': 100}, key=str(uuid.uuid1()))
+    return resp
